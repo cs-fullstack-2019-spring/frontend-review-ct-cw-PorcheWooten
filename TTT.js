9e6ebf54ef6f1c51b26dcd2e248562b08f3b7d9c
PorcheWooten <porchewooten@yahoo.com>
2019-02-05 16:24:05 -0600

    graded

---
+++ TTT.js
@@ -0,0 +1,58 @@
+var gameSpaces = document.querySelectorAll("td");
+var player1 = 1;
+
+
+function clickedSquare(e) {
+    console.log("hjh");
+
+    if (player1 === 1 && e.target.innerHTML ==="")
+    {
+        e.target.innerHTML = "X";
+        player1 = 2;
+    }
+    else if(e.target.innerHTML === "")
+    {
+        e.target.innerHTML = "O";
+        player1 = 1;
+    }
+    checkForWinner()
+}
+for (var i = 0; i < gameSpaces.length; i++) {
+    gameSpaces[i].addEventListener('click', clickedSquare);
+}
+//function to check for winner
+function checkForWinner() {
+console.log('Checking for winner');
+    if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="X")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="X")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X"))
+    {
+        alert("X wins");
+        return false;
+    }
+    else if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="O")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="O")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O"))
+    {
+        alert("O wins");
+        return false;
+    }
+
+}
+// Add a New Game button that when clicked, will clear the game area and start a new game.
+var btn = document.querySelector("button");
+btn.onclick=function(e){
+    console.log('button clicked');
+    for (var i = 0; i < gameSpaces.length; i++)
+        gameSpaces[i].innerHTML="";
+};
